c86f79f419ffcd1c74097a3f16e196445ffaafb5
bagus2626 <bagusajalah19@gmail.com>
2024-07-11 23:04:17 +0700

    hapus informasi seputar pelayanan

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -249,55 +249,7 @@
     </section>
 
       <!-- Services Section -->
-      <section id="services" class="services section light-background">
-
-        <!-- Section Title -->
-        <div class="container section-title" data-aos="fade-up">
-          <h2>Informasi seputar pelayanan</h2>
-        </div><!-- End Section Title -->
-
-        <div class="container">
-
-          <div class="row gy-4">
-
-            <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
-              <div class="service-item position-relative">
-                <div class="icon"><i class="bi bi-speedometer2 icon"></i></div>
-                <h4><a href="" class="stretched-link">Informasi Perizinan</a></h4>
-                <p>Informasi seputar masing-masing perizinan yang ada</p>
-              </div>
-            </div><!-- End Service Item -->
-
-            <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="200">
-              <div class="service-item position-relative">
-                <div class="icon"><i class="bi bi-globe icon"></i></div>
-                <h4><a href="" class="stretched-link">Layanan Perizinan Berusaha</a></h4>
-                <p>Perizinan Berusaha Berbasis Risiko</p>
-              </div>
-            </div><!-- End Service Item -->
-
-            <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="300">
-              <div class="service-item position-relative">
-                <div class="icon"><i class="bi bi-file-text icon"></i></div>
-                <h4><a href="" class="stretched-link">Layanan Pengaduan</a></h4>
-                <p>Anda dapat melakukan pengaduan terhadap pelayanan kami</p>
-              </div>
-            </div><!-- End Service Item -->
-
-            <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="400">
-              <div class="service-item position-relative">
-                <div class="icon"><i class="bi bi-globe-asia-australia icon"></i></div>
-                <h4><a href="" class="stretched-link">Jadwal Konsultasi</a></h4>
-                <p>Pemesanan jadwal untuk konsultasi secara tatap muka</p>
-              </div>
-            </div><!-- End Service Item -->
-
-          </div>
-
-        </div>
-
-      </section><!-- /Services Section -->
-
+    
     <!-- Why Us Section -->
     <section id="why-us" class="section why-us light-background" data-builder="section">
 
