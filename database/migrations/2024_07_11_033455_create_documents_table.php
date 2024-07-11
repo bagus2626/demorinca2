ccc3b2a63072e2db05c17543a0e0f15d0b88c3c2
ivan-adi-saputra <ivanadisaputra34@gmail.com>
2024-07-11 10:53:51 +0700

    setup laravel

---
+++ database/migrations/2024_07_11_033455_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->string('token');
+            $table->string('no_registrasi_sistem_simbg');
+            $table->string('nama_pemohon');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
