11276991e40e795fb4ec4d757453cdd258c3236d
Ghofir <ghofir.jz4@gmail.com>
2024-07-21 14:43:47 +0700

    artikrel 1

--- resources/views/dashboard/artikelpage1.blade.php
+++ resources/views/dashboard/artikelpage1.blade.php
@@ -24,10 +24,47 @@
 <section id="artikel1" class="section why-us light-background">
   <div class="container">
     <div class="row gy-4">
-      <div class="col-lg-6 content" data-aos="fade-right" data-aos-delay="100" data-aos-duration="800">
-        <h4>
-          Artikel 1
-        </h4>
+      <div class="col-lg-12 content" data-aos="fade-right" data-aos-delay="100" data-aos-duration="800">
+        <h2 class="text-center">
+          Konsultan Pengurusan Perizinan Bangunan berpengalaman dan berlisensi
+        </h2>
+        <p class="">
+          PT. Rinca Cipta Karya merupakan Konsultan Pengurusan Perizinan Bangunan berpengalaman yang sudah berlisensi dan juga melayani konsultan perencanaan arsitek di seluruh indonesia hingga internasional.
+          <br>
+          PT Rinca Cipta Karya juga merupakan konsultan yang diproyeksikan menjadi salah satu terbaik di Indonesia, Seperti Visi dan Misi Kami yang selalu senantiasa memegang teguh filosofi untuk "PASTI MEMBERIKAN JASA TERBAIK UNTUK SELURUH KLIEN", dengan penuh Amanah, Kompeten, Kolaboratif, Adaptif, dan Loyal.
+          Kami adalah perusahaan yang bergerak dibidang Konsultan Perizinan Bangunan yang berpusat di wilayah Kota Depok Provinsi Jawa Barat.
+
+          <h3 class="text-center">Layanan PT. Rinca Cipta Karya</h3><br>
+          Kami memiliki Layanan yang mungkin Anda butuhkan, seperti : <br>          
+          1. Perencanaan tata ruang, bangunan dan industri : <br>
+
+            Perencanaan tata ruang : Melakukan analisis dan perencanaan tata ruang yang mencakup aspek lingkungan, sosial, dan ekonomi untuk memastikan pembangunan yang berkelanjutan dan terintegrasi. <br>
+            Perencanaan Bangunan : Membuat rencana arsitektural dan struktur bangunan yang sesuai dengan kebutuhan dan regulasi yang berlaku. <br>
+            Perencanaan Industri: Merancang tata letak dan struktur industri untuk efisiensi operasional dan kepatuhan terhadap standar keamanan dan lingkungan. <br><br>
+
+          2. Perizinan bangunan gedung dan industri<br>
+
+              Izin Mendirikan Bangunan (IMB) : Proses mendapatkan izin untuk mendirikan bangunan baru atau melakukan perubahan pada bangunan yang ada. <br>
+              Sertifikat Laik Fungsi (SLF) : Sertifikasi yang menyatakan bahwa bangunan telah memenuhi persyaratan teknis dan laik digunakan. <br>
+              Izin Lingkungan : Mendapatkan izin yang diperlukan untuk memastikan bahwa proyek pembangunan atau industri tidak merusak lingkungan sekitar. <br>
+              Izin Operasional Industri : Mengurus semua dokumen yang diperlukan untuk memulai dan menjalankan operasi industri sesuai dengan peraturan yang berlaku. <br> <br>
+
+          3. Desain Interior <br><br>
+
+          4. Desain Konsep : <br>
+              Merancang konsep desain interior yang sesuai dengan keinginan klien dan fungsi ruang.<br> <br>
+
+          5. Pemilihan Material : <br>
+              Membantu dalam pemilihan material yang berkualitas dan sesuai dengan tema desain.<br><br>
+
+          6. Pengawasan Pelaksanaan : <br>
+              Memastikan bahwa proses pelaksanaan desain interior berjalan sesuai dengan rencana dan spesifikasi yang telah disepakati.<br><br>
+
+          7. Furnishing : <br>
+              Menyediakan pilihan furnitur dan dekorasi yang melengkapi desain interior dan memberikan kenyamanan serta estetika yang diinginkan.<br><br>
+
+           Jangan sampai salah memilih Konsultan Pengurusan Perizinan Bangunan. Percayakan Pada Kami PT. Rinca Cipta Karya.<br>
+        </p>
       </div>
       
   </div>
